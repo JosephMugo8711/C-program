767c9ad048086fde3281f7c145ffb3d345bd125d
wamiti8711@gmail.com <wamiti8711@gmail.com>
2024-01-26 08:02:17 +0300

    Feat: c

--- week1/hello.c
+++ week1/hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <cs50.h>
+
 
 int main(void)
 {
-    string answer = get_string("What's your name? ");
-    printf("hello, %s\n", answer);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("meow\n");
+    }
 }
