5bba01151c1c6351aca966bbcac59be033e9be72
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-08 08:57:15 +0300

    Feat: swap function

--- week4/snap.c
+++ week4/snap.c
@@ -1,21 +1,38 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// void swap(int a, int b);
+// Fun prototype for swap function
+void swap(int *a, int *b);
 
-// int main(void)
-// {
-//     int x = 1;
-//     int y = 2;
+// Main Function
+int main(void)
+{
+    //  Declare and initialize variables x and y
+    int x = 1;
+    int y = 2;
 
-//     printf("x is %i, y is %i\n", x, y);
-//     swap(x, y);
-//     printf("x is %i, y is %i\n", x, y);
 
-// }
+    // Print initial values of x and y
+    printf("x is %i, y is %i\n", x, y);
+    // Call swap function to swap values of x and y
+    swap(&x, &y);
 
-// void swap(int a, int b)
-// {
-//     int tmp = a;
-//     a = b;
-//     b = tmp;
-// }
+    // Print values of x and y after swap
+    printf("x is %i, y is %i\n", x, y);
+
+    // Return 0 to indicate successful execuion
+    return 0;
+
+}
+
+// Function definition for swap function
+void swap(int *a, int *b)
+{
+    // Temporary variable to hold the value of a
+    int tmp = *a;
+
+    // Assign value of b to a
+    *a = *b;
+
+    // Assign value of temporary variable to b
+    *b = tmp;
+}
