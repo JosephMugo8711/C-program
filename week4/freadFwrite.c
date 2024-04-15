016c555c68d1e431f7bc570e7d15bf781288196a
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-15 08:29:13 +0300

    read,write, open, close files

---
+++ week4/freadFwrite.c
@@ -0,0 +1,10 @@
+//fread - reads data from a file into a buffer
+// fwrite - writes data from a buffer* to a file
+
+
+// A buffer is a chunk of memory that can temporarily store some data
+
+//fread(..., ..., ....,  ..from where are we trying to read)
+//fread(..., ..., ....,  f);
+//fread(buffer ..., ....,  f); // read into the buffer
+// where are we going to get the data, where are we going to store
---
+++ week4/openClose.c
@@ -0,0 +1,9 @@
+// fopen- opens a file for future reading / writing
+// fclose - closes a file
+
+// FILE *f = fopen("hi.txt", "r");
+//fopen returns - 
+// f - find files to open them
+
+//fclose(f) - point to that file and close it
+
